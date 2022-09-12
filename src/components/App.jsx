852ab5826cab448734fda39dd8ade7a3d7b1ec45
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-09-12 17:04:41 +0300

    Added additional input data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
-import user from 'data/user.json';
 import { Profile } from 'components/AllComponents';
+import userData from 'data/user.json';
+import statisticsData from 'data/data.json';
+import friendsData from 'data/friends.json';
 import { Profile, Statistics, FriendList } from 'components/AllComponents';
 
 export const App = () => {
