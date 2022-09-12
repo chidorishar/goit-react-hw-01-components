b8f388772677fd5a2372dd883d2f2b954b8e8c6c
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-09-12 22:30:35 +0300

    Added theme environment, deleted old solution

---
+++ src/components/common/Theme/Theme.styled.jsx
@@ -0,0 +1,19 @@
+const theme = {
+  colors: {
+    textDark: '#121212',
+    textGrey: '#9b9b9b',
+    backgroundStats: '#f6faff',
+    backgroundAvatar: '#c89f36',
+  },
+  shadows: {
+    medium: '2px 10px 20px #b6b6b6',
+  },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
+  fontWeights: {
+    body: 400,
+    medium: 500,
+    bold: 700,
+  },
+};
+
+export { theme };
