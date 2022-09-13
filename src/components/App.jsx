601eb1d1cd82d5cdbb675b2c6f3291a44dbe3630
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-09-13 09:10:21 +0300

    Corrected ThemeProvider usage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import userData from 'data/user.json';
 import statisticsData from 'data/data.json';
 import friendsData from 'data/friends.json';
 import transactionData from 'data/transactions.json';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from 'components/common/Theme/Theme.styled';
 import {
   Profile,
   Statistics,
@@ -12,17 +14,19 @@ import {
 export const App = () => {
   return (
     <>
-      <Profile
-        username={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        avatar={userData.avatar}
-        stats={userData.stats}
-      />
-      <Statistics title="Upload stats" stats={statisticsData} />
-      <Statistics stats={statisticsData} />
-      <FriendList friends={friendsData} />
-      <TransactionHistory items={transactionData} />
+      <ThemeProvider theme={theme}>
+        <Profile
+          username={userData.username}
+          tag={userData.tag}
+          location={userData.location}
+          avatar={userData.avatar}
+          stats={userData.stats}
+        />
+        <Statistics title="Upload stats" stats={statisticsData} />
+        <Statistics stats={statisticsData} />
+        <FriendList friends={friendsData} />
+        <TransactionHistory items={transactionData} />
+      </ThemeProvider>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from '@emotion/react';
-import { theme } from 'components/common/Theme/Theme.styled';
 import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
