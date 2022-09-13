a93c41732f6709b8f6e84d448b34272f82ebd7a6
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-09-13 15:22:07 +0300

    Updated theme

--- src/components/common/Theme/Theme.styled.jsx
+++ src/components/common/Theme/Theme.styled.jsx
@@ -1,12 +1,19 @@
+import { getQueriesForElement } from '@testing-library/react';
+
 const theme = {
   borders: {
     small: '1px solid #cdcdcd',
   },
   colors: {
+    textLight: '#ececec',
     textDark: '#121212',
     textGrey: '#9b9b9b',
     backgroundStats: '#f6faff',
     backgroundAvatar: '#c89f36',
+    onlineStatus: {
+      online: '#008000',
+      offline: '#ff0000',
+    },
   },
   fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
   fontWeights: {
@@ -20,11 +27,13 @@ const theme = {
     round: '50%',
   },
   sizes: {
+    cardSmall: '200px',
     cardNormal: '300px',
-    cardWide: '600px',
+    cardWide: '500px',
   },
   space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
   shadows: {
+    small: '3px 5px 9px 0px #b6b6b6',
     medium: '2px 10px 20px #b6b6b6',
   },
 };
